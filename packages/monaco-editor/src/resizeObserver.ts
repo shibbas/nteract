8bb7085c01865a554aa013afa789649602ff43dd
Bryan Chen <brchen@microsoft.com>
2023-02-10 16:18:01 -0800

    fix: defer the creation of resize observer

--- packages/monaco-editor/src/resizeObserver.ts
+++ packages/monaco-editor/src/resizeObserver.ts
@@ -15,32 +15,40 @@ export interface IResizable {
  * ResizeObserver that monitors the size of the element and calls the resizable component.
  */
 const monitoredResizables = new Map<Element, IResizable>();
-const ResizeObserverImpl = window.ResizeObserver || ResizeObserverPolyfill;
 
 /**
  * ResizeObserver that monitors the size of the element and calls the resizable component.
  */
-const resizeObserver = new ResizeObserverImpl((entries) => {
-  for (const entry of entries) {
-    if (monitoredResizables.has(entry.target)) {
-      const editor = monitoredResizables.get(entry.target);
-      editor?.onResize();
-    }
+let resizeObserver: ResizeObserver;
+function getResizeObserverSingleton() {
+  if (!resizeObserver) {
+    const ResizeObserverImpl = window.ResizeObserver || ResizeObserverPolyfill;
+
+    resizeObserver = new ResizeObserverImpl((entries) => {
+      for (const entry of entries) {
+        if (monitoredResizables.has(entry.target)) {
+          const editor = monitoredResizables.get(entry.target);
+          editor?.onResize();
+        }
+      }
+    });
   }
-});
+
+  return resizeObserver;
+}
 
 /**
  * Observe the element for resize events.
  */
 export function observe(resizable: IResizable, element: HTMLDivElement) {
   monitoredResizables.set(element, resizable);
-  resizeObserver.observe(element);
+  getResizeObserverSingleton().observe(element);
 }
 
 /**
  * Unobserve the element for resize events.
  */
 export function unobserve(element: HTMLDivElement) {
-  resizeObserver.unobserve(element);
+  getResizeObserverSingleton().unobserve(element);
   monitoredResizables.delete(element);
 }
